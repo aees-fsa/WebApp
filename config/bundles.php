4ac2669d0fe6aaa53ac14481644340d6f7200877
Andrew SASSOYE <andrew@sassoye.be>
2020-04-07 12:35:09 +0200

    composer require symfony/security-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -24,4 +24,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
