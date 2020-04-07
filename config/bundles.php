38984641191efac621ef49c9e9384fd190230d41
Andrew SASSOYE <andrew@sassoye.be>
2020-04-07 12:33:27 +0200

    composer require annotations

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
